ab18e34ebba8310b12ac98a916421ecc002d76b9
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2022-11-18 19:47:54 -0500

    Created Layout for Groups pages

--- src/pages/ViewGroups/GroupCard.jsx
+++ src/pages/ViewGroups/GroupCard.jsx
@@ -6,29 +6,38 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { flexbox } from "@mui/system";
-import Grid from '@mui/material/Grid'
-import GroupImage from "../../Assets/GroupImage.png"
+import Grid from "@mui/material/Grid";
+import GroupImage from "../../Assets/GroupImage.png";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-
-
-
-export default function GroupdCard({id, title, description, image}) {
+export default function GroupdCard({ id, name, description, splash, members, location }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 380 }} className="groupCard">
       <CardMedia
         component="img"
         alt="Group Image"
-        height="140"
-        image= {GroupImage}
+        height="190"
+        image={GroupImage}
       />
       <CardContent>
         <Grid container>
-          <Grid item xs={6}>
+          <Grid item xs={12} className="cardHeader">
             <Typography gutterBottom variant="h5" component="div">
-              {title}
+              {name}
             </Typography>
           </Grid>
-          <Grid item xs={6}>
+        </Grid>
+        <Grid container>
+          <Grid item xs={4}>
+            <Typography gutterBottom variant="h7" component="div">
+            <LocationOnIcon/> {location}
+            </Typography>
+            <Typography gutterBottom variant="body2" component="div">
+              <AccountCircleIcon/>{members} members
+            </Typography>
+          </Grid>
+          <Grid item xs={8}>
             <Typography variant="body2" color="text.secondary">
               {description}
             </Typography>
--- src/pages/ViewGroups/Groups.jsx
+++ src/pages/ViewGroups/Groups.jsx
@@ -1,41 +1,105 @@
 import React from "react";
 import GroupdCard from "./GroupCard";
-
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import styles from "./Groups.scss";
 
 const info = [
   {
     id: 1,
-    title: "Project1",
-    description: "Dorem ipsum lolololol",
-    image : "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"
+    name: "Project1",
+    description:
+      "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    splash:
+      "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg",
+    location: "Miami, FL",
+    members: 12,
   },
   {
     id: 2,
-    title: "Project2",
-    description: "Dorem ipsum lolololol",
-    image : "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"
+    name: "Project2",
+    description:
+      "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    splash:
+      "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg",
+    location: "Miami, FL",
+    members: 12,
   },
   {
     id: 3,
-    title: "Project3",
-    description: "Dorem ipsum lolololol",
-    image : "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"
+    name: "Project3",
+    description:
+      "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    splash:
+      "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg",
+    location: "Miami, FL",
+    members: 12,
   },
   {
     id: 4,
-    title: "Project4",
-    description: "Dorem ipsum lolololol",
-    image : "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"
+    name: "Project4",
+    description:
+      "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    splash:
+      "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg",
+    location: "Miami, FL",
+    members: 12,
+  },
+  {
+    id: 4,
+    name: "Project4",
+    description:
+      "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    splash:
+      "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg",
+    location: "Miami, FL",
+    members: 12,
   },
 ];
 
 const Groups = () => {
   return (
-    <div>
-      {info.map(({ id, title, description}) => {
-        return <GroupdCard title={title} description={description}/>;
-      })}
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid
+        container
+        spacing={1}
+        columnSpacing={1}
+        alignItems="stretch"
+        className="bodySection"
+      >
+        <Grid className="Dummy" item xs={4}>
+          xs=4
+        </Grid>
+        <Grid
+          container
+          xs={8}
+          columnSpacing={4}
+          rowSpacing={4}
+          justifyContent="center"
+        >
+          {info.map(({ id, name, description, splash, members, location }) => {
+            return (
+              <Grid item xs="auto" justifyContent="flex-end">
+                <GroupdCard
+                  name={name}
+                  description={description}
+                  members={members}
+                  location={location}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Grid>
+    </Box>
+
+    // <div>
+    //   {info.map(({ id, title, description}) => {
+    //     return <GroupdCard title={title} description={description}/>;
+    //   })}
+    // </div>
   );
 };
 
