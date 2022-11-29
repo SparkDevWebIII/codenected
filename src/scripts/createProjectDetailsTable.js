9b6b5043aa7b06808375e03835a9309becc6e60c
corey duke <cdukedev@gmail.com>
2022-11-28 22:28:30 -0500

    Added secondary keys for the project details dynamo table

--- src/scripts/createProjectDetailsTable.js
+++ src/scripts/createProjectDetailsTable.js
@@ -1,7 +1,7 @@
 const AWS = require("aws-sdk");
 
 AWS.config.update({
-  region: "us-east-1"
+  region: "us-east-1",
 });
 
 const dynamodb = new AWS.DynamoDB();
@@ -12,22 +12,37 @@ const params = {
     // Partition Key
     { AttributeName: "project_id", KeyType: "HASH" },
     // Sort Keys
-    { AttributeName: "title", KeyType: "RANGE"}  
+    { AttributeName: "title", KeyType: "RANGE" },
   ],
   AttributeDefinitions: [
     { AttributeName: "project_id", AttributeType: "S" },
     { AttributeName: "title", AttributeType: "S" },
   ],
- 
+  LocalSecondaryIndexes: [
+    {
+      IndexName: "AltIndex",
+      KeySchema: [
+        { AttributeName: "project_id", KeyType: "HASH" },
+        { AttributeName: "title", KeyType: "RANGE" },
+      ],
+      Projection: {
+        ProjectionType: "KEYS_ONLY",
+      },
+    },
+  ],
+
   ProvisionedThroughput: {
     ReadCapacityUnits: 10,
-    WriteCapacityUnits: 10
-  }
+    WriteCapacityUnits: 10,
+  },
 };
 
-dynamodb.createTable(params, function(err, data) {
+dynamodb.createTable(params, function (err, data) {
   if (err)
-    console.error("Unable to create table: ", JSON.stringify(err, null, 2))
+    console.error("Unable to create table: ", JSON.stringify(err, null, 2));
   else
-    console.log("Created table with description: ", JSON.stringify(data, null, 2))
+    console.log(
+      "Created table with description: ",
+      JSON.stringify(data, null, 2)
+    );
 });
