b5b3858586077a3e6235d924396bb3e3fb0dda46
corey duke <cdukedev@gmail.com>
2022-12-01 10:13:40 -0500

    uodated load function for member details

---
+++ src/scripts/loadMemberDetail.js
@@ -0,0 +1,43 @@
+const AWS = require("aws-sdk");
+const fs = require("fs");
+
+AWS.config.update({
+  region: "us-east-1",
+});
+
+console.log("Writing entries to Member Detail table.");
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const memberData = JSON.parse(
+  fs.readFileSync("../../public/data/memberDetail.json", "utf8")
+);
+
+memberData.forEach(function(memberDetail) {
+  const params = {
+    TableName: "memberDetail",
+    Item: {
+      member_id: memberDetail.member_id,
+      name: memberDetail.name,
+      about: memberDetail.about,
+      position: memberDetail.position,
+      location: memberDetail.location,
+      dateCreated: memberDetail.dateCreated,
+      email: memberDetail.email,
+      skills: memberDetail.skills,
+      projects: memberDetail.projects,
+      groups: memberDetail.groups,
+      profileImage: memberDetail.profileImage,
+    },
+  };
+  console.log(params.Item);
+  dynamodb.put(params, (err, data) => {
+    if (err)
+      console.error(
+        "Unable to load data into table",
+        memberDetail.title,
+        ". Error: ",
+        JSON.stringify(err, null, 2)
+      );
+    else console.log("Added", memberDetail.title, "to table.");
+  });
+});
