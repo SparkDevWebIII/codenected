f62d3c731611ec00593f8ce1f2a4875767726130
corey duke <cdukedev@gmail.com>
2022-12-01 06:55:38 -0500

    updated and finished connecting project details api

--- src/scripts/createProjectDetailsTable.js
+++ src/scripts/createProjectDetailsTable.js
@@ -37,7 +37,7 @@ const params = {
   },
 };
 
-dynamodb.createTable(params, function (err, data) {
+dynamodb.createTable(params, function(err, data) {
   if (err)
     console.error("Unable to create table: ", JSON.stringify(err, null, 2));
   else
--- src/scripts/loadProjectDetail.js
+++ src/scripts/loadProjectDetail.js
@@ -1,43 +1,49 @@
 const AWS = require("aws-sdk");
-const fs = require('fs');
+const fs = require("fs");
 
 AWS.config.update({
-  region: "us-east-1"
+  region: "us-east-1",
 });
 
-console.log("Writing entries to Project table.");
+console.log("Writing entries to Project Detail table.");
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
-const projectData = 
-  JSON.parse(fs.readFileSync('../../public/data/projectDetail.json', 'utf8'));
+const projectData = JSON.parse(
+  fs.readFileSync("../../public/data/projectDetail.json", "utf8")
+);
 
 projectData.forEach(function(projectDetail) {
   const params = {
     TableName: "projectDetail",
     Item: {
-        "project_id": projectDetail.project_id,
-        "title": projectDetail.title,
-        "description": projectDetail.description,
-        "location" : projectDetail.location,
-        "url" : projectDetail.url,
-        "startDate": projectDetail.startDate,
-        "endDate": projectDetail.endDate,
-        "status": projectDetail.status,
-        "manager": projectDetail.manager,
-        "technologyUsed": projectDetail.technologyUsed,
-        "image": projectDetail.image,
-        "imageAlt": projectDetail.imageAlt,
-        "projectDetails": projectDetail.projectDetails,
-        "leadership": projectDetail.leadership,
-        "members": projectDetail.members
-    }
+      project_id: projectDetail.project_id,
+      title: projectDetail.title,
+      projectImage: projectDetail.projectImage,
+      projectImageAlt: projectDetail.projectImageAlt,
+      websiteUrl: projectDetail.websiteUrl,
+      summary: projectDetail.summary,
+      location: projectDetail.location,
+      dateCreated: projectDetail.dateCreated,
+      email: projectDetail.email,
+      githubRepo: projectDetail.githubRepo,
+      concept: projectDetail.concept,
+      goal: projectDetail.goal,
+      currentsystem: projectDetail.currentsystem,
+      technologies: projectDetail.technologies,
+      rolesNeeded: projectDetail.rolesNeeded,
+      members: projectDetail.members,
+      projectProgression: projectDetail.projectProgression,
+    },
   };
-
+  console.log(params.Item);
   dynamodb.put(params, (err, data) => {
     if (err)
-      console.error("Unable to load data into Projects table",
-                    projectDetail.title, ". Error: ", JSON.stringify(err, null, 2))
-    else
-      console.log("Added", projectDetail.title, "to project table.")
+      console.error(
+        "Unable to load data into table",
+        projectDetail.title,
+        ". Error: ",
+        JSON.stringify(err, null, 2)
+      );
+    else console.log("Added", projectDetail.title, "to table.");
   });
 });
