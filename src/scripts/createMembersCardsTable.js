5b3fa55cb18aa5b1fb0772537cd8969925f909f8
corey duke <cdukedev@gmail.com>
2022-12-01 07:50:17 -0500

    added the create members table

--- src/scripts/createMembersCardsTable.js
+++ src/scripts/createMembersCardsTable.js
@@ -1,33 +1,47 @@
 const AWS = require("aws-sdk");
 
 AWS.config.update({
-  region: "us-east-1"
+  region: "us-east-1",
 });
 
 const dynamodb = new AWS.DynamoDB();
 
 const params = {
-  TableName: "users",
+  TableName: "membersCards",
   KeySchema: [
     // Partition Key
-    { AttributeName: "user_id", KeyType: "HASH" },
+    { AttributeName: "member_id", KeyType: "HASH" },
     // Sort Keys
-    { AttributeName: "name", KeyType: "RANGE"}  
+    { AttributeName: "name", KeyType: "RANGE" },
   ],
   AttributeDefinitions: [
-    { AttributeName: "user_id", AttributeType: "S" },
+    { AttributeName: "member_id", AttributeType: "S" },
     { AttributeName: "name", AttributeType: "S" },
   ],
- 
+  LocalSecondaryIndexes: [
+    {
+      IndexName: "AltIndex",
+      KeySchema: [
+        { AttributeName: "member_id", KeyType: "HASH" },
+        { AttributeName: "name", KeyType: "RANGE" },
+      ],
+      Projection: {
+        ProjectionType: "KEYS_ONLY",
+      },
+    },
+  ],
   ProvisionedThroughput: {
     ReadCapacityUnits: 10,
-    WriteCapacityUnits: 10
-  }
+    WriteCapacityUnits: 10,
+  },
 };
 
 dynamodb.createTable(params, function(err, data) {
   if (err)
-    console.error("Unable to create table: ", JSON.stringify(err, null, 2))
+    console.error("Unable to create table: ", JSON.stringify(err, null, 2));
   else
-    console.log("Created table with description: ", JSON.stringify(data, null, 2))
+    console.log(
+      "Created table with description: ",
+      JSON.stringify(data, null, 2)
+    );
 });
