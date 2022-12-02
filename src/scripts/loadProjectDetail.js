615cfcda76b1dfc8050bf28499371d90403026cd
corey duke <cdukedev@gmail.com>
2022-12-02 14:27:46 -0500

    added rolesNeed as an array of objects

--- src/scripts/loadProjectDetail.js
+++ src/scripts/loadProjectDetail.js
@@ -12,28 +12,6 @@ const projectData = JSON.parse(
   fs.readFileSync("../../public/data/projectDetail.json", "utf8")
 );
 
-// "rolesNeeded": [
-//   {
-//     "position": "Project Manager",
-//     "domain": "Software Development",
-//     "description": "The project manager is responsible for the overall success of the project. They are responsible for the project schedule, budget, and quality. They are also responsible for the project team and the project environment. The project manager is the primary point of contact for the client and the project team.",
-//   },
-//   {
-//     "position": "Software Developer",
-//     "domain": "Software Development",
-//     "description": "The software developer is responsible for the development of the software. They are responsible for the design, implementation, and testing of the software. They are also responsible for the documentation of the software.",
-//   },
-//   {
-//     "position": "Software Tester",
-//     "domain": "Software Development",
-//     "description": "The software tester is responsible for the testing of the software. They are responsible for the testing of the software and the documentation of the testing results. They are also responsible for the testing environment.",
-//   },
-//   {
-//     "position": "Software Architect",
-//     "domain": "Software Development",
-//     "description": "The software architect is responsible for the design of the software. They are responsible for the design of the software and the documentation of the design. They are also responsible for the design environment.",
-//   }
-// ]
 projectData.forEach(function(projectDetail) {
   const params = {
     TableName: "projectDetail",
